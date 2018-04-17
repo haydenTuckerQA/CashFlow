e32b1df192f28e6d38bdec42b81331f12735d647
tomwoen <thomas.owen@qa.com>
2018-04-17 15:36:57 +0100

    added maven project

---
+++ Cashflow/src/main/java/Cashflow/Cashflow/App.java
@@ -0,0 +1,13 @@
+package Cashflow.Cashflow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
