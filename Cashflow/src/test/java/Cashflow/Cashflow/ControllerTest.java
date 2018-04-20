d042bb82c80f208a7dca46de70a7c9b3277caec1
tomwoen <thomas.owen@qa.com>
2018-04-20 14:23:47 +0100

    Added a test for requesting information from the database

---
+++ Cashflow/src/test/java/Cashflow/Cashflow/ControllerTest.java
@@ -0,0 +1,26 @@
+package Cashflow.Cashflow;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith (SpringRunner.class)
+
+public class ControllerTest {
+	
+	
+	private DatabaseReader databasereader;
+
+	@Test
+	public void databaseReaderTest() {
+		
+		databasereader = new DatabaseReader();
+		String expectedResult = "{\"Transaction_ID\":\"1\",\"Amount\":\"£2000\",\"Date\":\"1/12/01\",\"In_or_Out\":\"In\",\"Type:\";\"Credit\",\"Description\";\"Monthly Wage\",\"Transaction_ID\";\"1\"}";
+		String actualResult = databasereader.getJSON();
+		
+		
+	}
+
+}
