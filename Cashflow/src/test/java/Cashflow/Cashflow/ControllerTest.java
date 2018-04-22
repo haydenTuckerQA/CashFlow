4362700ed455067ee24c5c5aaa4ab883e54916d7
tmowen <thomas.owen@qa.com>
2018-04-22 20:40:48 +0100

    Attempted to improve first test

--- Cashflow/src/test/java/Cashflow/Cashflow/ControllerTest.java
+++ Cashflow/src/test/java/Cashflow/Cashflow/ControllerTest.java
@@ -2,6 +2,7 @@ package Cashflow.Cashflow;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -10,13 +11,17 @@ import org.junit.runner.RunWith;
 
 public class ControllerTest {
 	
-	
+	@InjectMocks
 	private DatabaseReader databasereader;
+	
+	@Before 
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
 
 	@Test
 	public void databaseReaderTest() {
 		
-		databasereader = new DatabaseReader();
 		String expectedResult = "{\"Transaction_ID\":\"1\",\"Amount\":\"£2000\",\"Date\":\"1/12/01\",\"In_or_Out\":\"In\",\"Type:\";\"Credit\",\"Description\";\"Monthly Wage\",\"Transaction_ID\";\"1\"}";
 		String actualResult = databasereader.getJSON();
 		assertEquals(expectedResult,actualResult);
